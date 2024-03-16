92578b158e96163ad5b62bbac050d24e771edb06
Sandhiya94 <sandhiyanatraj@gmail.com>
2024-03-16 13:30:58 -0400

    6th commit

--- crud.py
+++ crud.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def show_index():
-    return render_template('index.html')
+    # return render_template('index.html')
+   return "hello world"
 
 # Deleting tasks
 @app.route('/delete')
